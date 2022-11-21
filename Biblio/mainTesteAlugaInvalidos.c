aa96d6bffbbe652a07f5a90cc5f4b25ef6d9091a
João Victor Amorim <69279163+Peisr25@users.noreply.github.com>
2022-11-21 17:24:32 -0300

    alterado o parametro matr para char

--- Biblio/mainTesteAlugaInvalidos.c
+++ Biblio/mainTesteAlugaInvalidos.c
@@ -11,8 +11,8 @@ int tests_run = 1;
 static char* testesInvalidos(void) {
     puts("Iniciando Testes Validos!!\n");
 
-    mu_assert("Teste alugando um livro ja alugado \n", 0 == alugaLivro(NOMEARQ, 120, 2111295));
-    mu_assert("Teste alugando um livro que nao exite \n", 2 == alugaLivro(NOMEARQ, 122, 2111111));
+    mu_assert("Teste alugando um livro ja alugado \n", 0 == alugaLivro(NOMEARQ, 120, "2111295"));
+    mu_assert("Teste alugando um livro que nao exite \n", 2 == alugaLivro(NOMEARQ, 122, "2111111"));
 
     return 0;
 }
