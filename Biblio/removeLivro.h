f3b8c1d83194ad16288f58d653ad075cbcf9cfca
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 23:17:54 -0300

    Update removeLivro.h

--- Biblio/removeLivro.h
+++ Biblio/removeLivro.h
@@ -1,1 +1,9 @@
+/**
+ * @brief Remove livros do cadastro da biblioteca.
+ * 
+ * @param {char *nomeArq} Nome do arquivo com os cadastros.
+ * @param {int idRemover} Código único do livro a ser removido.
+ * @returns {int} 2: livro ja esta alugado ; 1: livro removido com sucesso; 0: livro não foi removido da base.
+ */
+
 int removeLivro(char *nomeArq, int idRemover);
