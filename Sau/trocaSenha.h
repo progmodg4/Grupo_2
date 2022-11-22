9beb7785285dc65908a88f3bc4217e6d6d3e8d2f
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 22:29:54 -0300

    Update trocaSenha.h

--- Sau/trocaSenha.h
+++ Sau/trocaSenha.h
@@ -1,1 +1,10 @@
+/**
+ * @brief Trcoa a senha de um usuário no arquivo com os cadastros de login.
+ * 
+ * @param {char *nomeArq} Nome do arquivo com os cadastros.
+ * @param {char *usuario} Matrícula do usuário.
+ * @param {char *senha_nova} Nova senha.
+ * @returns {void} .
+ */
+
 void trocaSenha(char *nomeArq, char *usuario, char *senha_nova);
