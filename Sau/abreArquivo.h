1f3d9f1bc4d9d4b37b97ba57005ca5636ede78fb
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 22:21:56 -0300

    Update abreArquivo.h

--- Sau/abreArquivo.h
+++ Sau/abreArquivo.h
@@ -1,1 +1,7 @@
+/**
+ * @param {char *file} Nome do arquivo a ser aberto.
+ * @param {char *mode} Modo de abertura.
+ * @returns {FILE pointer} Ponteiro para o arquivo aberto.
+ */
+
 FILE *openFile(const char *file, const char *mode);
