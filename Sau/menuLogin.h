66b367bd1e613d6c6937573497a09016fe416050
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 22:25:25 -0300

    Update menuLogin.h

--- Sau/menuLogin.h
+++ Sau/menuLogin.h
@@ -1,1 +1,10 @@
+/**
+ * @brief Interface para login e cadastro de usuários.
+ * 
+ * @param {int tipo_usuario} Tipo do usuário; 1: usuário comum; qualquer outro valor: funcionário.
+ * @param {int *check} Aponta para -1 (login falhou) por padrão; modifica-se para 0 caso o login seja feito com sucesso.
+ * @returns {char*} Matrícula inserida pelo usuário.
+ * @interface 1a tela: prompt para qual operação realizar; 2a tela : prompt para inserir matricula e senha.
+ */
+
 char* fazLogin(int tipo_usuario, int *check);
