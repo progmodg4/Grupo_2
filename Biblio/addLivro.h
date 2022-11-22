a5d407410d86e3f13db5aac958075f1de33e12f7
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 22:53:42 -0300

    Update addLivro.h

--- Biblio/addLivro.h
+++ Biblio/addLivro.h
@@ -1,1 +1,11 @@
+/**
+ * @brief Adiciona livros ao cadastro da biblioteca.
+ * 
+ * @param {char *nomeArq} Nome do arquivo com os cadastros.
+ * @param {int id} .
+ * @param {char *nomeLivro} Nome do Livro.
+ * @param {char *editora} Editora.
+ * @returns {int} 1: livro cadastrado com sucesso; 0: livro já existe na base.
+ */
+
 int addLivro(char *nomeArq, int id, char *nomeLivro, char *editora);
