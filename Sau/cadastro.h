bd091e3d27a4e4fcb3747ab41a5652b5b142f657
João Victor Amorim <69279163+Peisr25@users.noreply.github.com>
2022-11-21 22:27:04 -0300

    Update cadastro.h

--- Sau/cadastro.h
+++ Sau/cadastro.h
@@ -1,7 +1,7 @@
 /** 
  * @param {FILE *arq} Nome do arquivo com os cadastros.
  * @param {char *usuario} Matrícula inserida pelo usuário.
- * @param {char *senha} Senha inserida pelo usuário.
+ * @param {char *senha} Senha inserida pelo usuário
  * @returns {int} 1: para cadastro com sucesso; 2: se o par matrícula e senha ja esteja cadastrado; 0: caso já exista matrícula com outra senha.
  */
 
