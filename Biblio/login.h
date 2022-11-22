b1517cd920f021d8a7580ecef7cf267c4f2e5b62
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 22:36:47 -0300

    Update login.h

--- Biblio/login.h
+++ Biblio/login.h
@@ -1,1 +1,14 @@
+/**
+ * @brief Usa funcao de busca para encontrar se o login (par matrícula e senha) inserido existe dentro do arquivo com os cadastros de login.
+ * 
+ * @param {FILE *arq} Nome do arquivo com todos os cadastros de login.
+ * @param {char *usuario} Matrícula inserida pelo usuário.
+ * @param {char *senha} Senha inserida pelo usuário.
+ * @returns {int} 0: caso encontrar; 1: caso não encontrar.
+ * 
+ * OBS: na main
+ * 1: enviara mensagem de erro "login ou senha invalido"
+ * 0: entrara na aplicacao
+ */
+
 int buscaArq(FILE *arq, char *usuario, char *senha);
