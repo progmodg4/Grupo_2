7413c04f5145a630ecfccb0b0d38c7a0def8297e
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 22:58:02 -0300

    Update alugaLivro.h

--- Biblio/alugaLivro.h
+++ Biblio/alugaLivro.h
@@ -1,1 +1,9 @@
+/**
+ * @brief Registra o aluguel de livros da biblioteca.
+ * 
+ * @param {char *nomeArq} Nome do arquivo com os cadastros.
+ * @param {int id} código único do livro.
+ * @returns {int} 2: livro não encontrado; 1: livro disponível para o aluguel; 0: livro existe no cadastro da biblioteca mas está indisponível.
+ */
+
 int alugaLivro(char* nomeArq, int idLivro,char * matr);
