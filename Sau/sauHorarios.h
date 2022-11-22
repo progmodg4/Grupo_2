ce48183706f0ca9de74d43a87b6549eab8781ccd
LucasToscanou <92105205+LucasToscanou@users.noreply.github.com>
2022-11-21 22:28:50 -0300

    Update sauHorarios.h

--- Sau/sauHorarios.h
+++ Sau/sauHorarios.h
@@ -1,1 +1,15 @@
+/**
+ * @brief Fornece a relação de disciplinas e seus horários em que o aluno ou professor está inscrito.
+ * 
+ * @param {int tipo_usuario} Tipo do usuário; 1: usuário comum; qualquer outro valor: professor.
+ * @param {char *matr} Matrícula do usuário.
+ * @returns {void} .
+ * @interface Apresenta-se uma tabela de duas colunas, sendo a primeira coluna o nome da disciplina e a segunda coluna, o horário dela, caso
+ * a matrícula seja encontrada. 
+ * 
+ * Obs: formato do arquivo 
+ * mat, disci, horario 
+ * char, char, char
+ */
+
 void verHor(int tipo_usuario, char* matr);
